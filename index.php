37d2c1cc7df32dd0e23a331189b677faecebe39b
ThanhPT11 <phantatthanh38@gmail.com>
2018-08-11 16:54:58 +0700

    update them

--- index.php
+++ index.php
@@ -19,11 +19,11 @@ get_header();
 ?>
 
 <div style="" class="wrap">
-	<?php if ( is_home() && ! is_front_page() ) : ?>
+	<!-- <?php if ( is_home() && ! is_front_page() ) : ?>
 		<header class="page-header">
 			<h1 class="page-title"><?php single_post_title(); ?></h1>
 		</header>
-	<?php else : ?>
+	<?php else : ?> -->
 
 
 	<header  class="page-header">
